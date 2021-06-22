201d5361675e0a3ad127ee0a7d522ef65dd87ef7
mayconbalves <mayconmustaine@hotmail.com>
2021-06-22 15:07:16 -0300

    feat: create footer component

---
+++ src/components/footer/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import * as S from './styled'
+
+const Footer = () => {
+  return (
+    <S.Footer />
+  )
+}
+
+export default Footer
---
+++ src/components/footer/styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Footer = styled.footer`
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 50px;
+  background-color: #121212;
+  color: #fff;
+  display: flex;
+  justify-content: space-around;
+`
