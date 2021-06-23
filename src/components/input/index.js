88e3917d367fd2697b3f416ba42f1570fead24f3
mayconbalves <mayconmustaine@hotmail.com>
2021-06-23 10:18:34 -0300

    chore: add props to input component

--- src/components/input/index.js
+++ src/components/input/index.js
@@ -2,14 +2,22 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import * as S from './styled'
 
-const Input = ({ placeholder }) => {
+const Input = ({ placeholder, name, value, onChange }) => {
   return (
-    <S.Input placeholder={placeholder} />
+    <S.Input
+      name={name}
+      onChange={onChange}
+      placeholder={placeholder}
+      value={value}
+    />
   )
 }
 
 export default Input
 
 Input.propTypes = {
-  placeholder: PropTypes.string.isRequired
+  name: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  placeholder: PropTypes.string.isRequired,
+  value: PropTypes.string
 }
