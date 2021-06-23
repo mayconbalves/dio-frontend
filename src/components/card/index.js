3ced9c1752845f5ee999e8551cd8e5599ffdf7ed
mayconbalves <mayconmustaine@hotmail.com>
2021-06-23 08:43:26 -0300

    feat: implements like and deslike functionality

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import * as S from './styled'
 
 const Card = () => {
+  const [like, setLike] = useState(0)
+  const [deslike, setDeslike] = useState(0)
   return (
     <S.Container>
-      <h1>card</h1>
-      <S.Like />
-      <S.Dislike />
+      <S.DescriptionContainer>
+        <S.Description>card</S.Description>
+      </S.DescriptionContainer>
+      <S.IconContainer>
+        <S.Like onClick={() => setLike(like + 1)}/><S.Cont>{like}</S.Cont>
+      </S.IconContainer>
+      <S.IconContainer>
+        <S.Dislike onClick={() => setDeslike(deslike + 1)}/>
+        <S.Cont>{deslike}</S.Cont>
+      </S.IconContainer>
     </S.Container>
   )
 }
--- src/components/card/styled.js
+++ src/components/card/styled.js
@@ -3,14 +3,60 @@ import * as I from '@styled-icons/boxicons-regular'
 
 export const Container = styled.div`
   display: flex;
+  border: 1px solid #fff;
+  height: 50px;
+  width: 50%;
+  margin: 10px auto;
+`
+export const DescriptionContainer = styled.div`
+  border: 1px solid #fff;
+  display: flex;
+  align-items: center;
+  width: 80%;
+
+  @media (max-width: 768px) {
+    width: 70%;
+  }
+
+  @media (max-width: 420px) {
+    width: 60%;
+  }
+`
+export const Description = styled.p`
+  color: #fff;
+  font-size: 1.5rem;
+  padding: 10px;
+`
+
+export const IconContainer = styled.div`
+  border: 1px solid #fff;
+  display: flex;
+  justify-content: center;
+  width: 10%;
+  @media (max-width: 768px) {
+    width: 15%;
+  }
+
+  @media (max-width: 768px) {
+    width: 20%;
+  }
 `
 
 export const Like = styled(I.Like)`
   color: green;
   width: 20px;
+  cursor: pointer;
 `
 
 export const Dislike = styled(I.Dislike)`
   color: red;
   width: 20px;
+  cursor: pointer;
+`
+
+export const Cont = styled.p`
+  color: #fff;
+  display: flex;
+  align-items: center;
+  margin: 5px;
 `
