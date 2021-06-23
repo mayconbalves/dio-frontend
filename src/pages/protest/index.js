538288564137d21eb6d69e4e40ecf416d65161e5
mayconbalves <mayconmustaine@hotmail.com>
2021-06-23 10:29:27 -0300

    feat: create submit form method

--- src/pages/protest/index.js
+++ src/pages/protest/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Container from 'components/container'
 import Input from 'components/input'
 import Breadcrumb from 'components/breadcrumb'
@@ -6,14 +6,33 @@ import Card from 'components/card'
 import * as S from './styled'
 
 const Protest = () => {
+  const [values, setValues] = useState({ protest: '' })
+
+  const inputChange = (e) => {
+    const { name, value } = e.target
+
+    setValues({
+      ...values,
+      [name]: value
+    })
+  }
+
+  const submitForm = () => {
+    console.log(values)
+  }
   return (
     <Container>
       <Breadcrumb />
       <S.Main>
         <S.Form>
           <S.Title>Eu Protesto</S.Title>
-          <Input placeholder="Enviar protesto" />
-          <S.Button type='button'>Enviar</S.Button>
+          <Input
+            name="protest"
+            onChange={inputChange}
+            placeholder="Enviar protesto"
+            value={values.protest}
+          />
+          <S.Button onClick={submitForm} type='button'>Enviar</S.Button>
         </S.Form>
         <Card />
       </S.Main>
