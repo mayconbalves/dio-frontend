cb80f68de638cf6fcb4415165c48ace88b1810eb
mayconbalves <mayconmustaine@hotmail.com>
2021-06-23 10:19:08 -0300

    chore: implements disabled button

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,17 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Input from 'components/input'
 import Container from 'components/container'
 import * as S from './styled'
 
 const Home = () => {
+  const [values, setValues] = useState({ username: '' })
+
+  const inputChange = (e) => {
+    const { name, value } = e.target
+
+    setValues({
+      ...values,
+      [name]: value
+    })
+  }
   return (
     <Container>
       <S.Main>
         <S.Title>Digite seu nome</S.Title>
         <S.Form>
-          <Input placeholder="Nome de usuário"/>
-          <S.Button to='/protest'>
-            Acesse o sistema
+          <Input
+            name="username"
+            onChange={inputChange}
+            placeholder="Nome de usuário"
+            value={values.username}
+          />
+          <S.Button type="button" disabled={values.username.length < 1}>
+            <S.Redirect to='/protest' disabled={values.username.length < 1}>
+              Acesse o sistema
+            </S.Redirect>
           </S.Button>
         </S.Form>
       </S.Main>
