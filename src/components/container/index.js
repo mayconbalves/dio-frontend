50b4aba1392f3aa1884497a99a986d4416dcc953
mayconbalves <mayconmustaine@hotmail.com>
2021-06-24 16:52:03 -0300

    refactor: removed footer component

--- src/components/container/index.js
+++ src/components/container/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Header from 'components/header'
-import Footer from 'components/footer'
 import * as S from './styled'
 
 const Container = ({ children }) => {
@@ -9,7 +8,6 @@ const Container = ({ children }) => {
     <S.Container>
       <Header />
       {children}
-      <Footer />
     </S.Container>
   )
 }
